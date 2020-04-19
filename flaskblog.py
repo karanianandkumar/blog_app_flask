14aeafe1bd1d55900eca034e1471acca1eb8cabb
Anand Kumar <anand.k@iaaatech.com>
2020-04-19 17:00:37 +0530

    adding routes

--- flaskblog.py
+++ flaskblog.py
@@ -1,12 +1,20 @@
 from flask import Flask
 app = Flask(__name__)
+#to use "flask run" set below environment variables
 #export FLASK_APP=flaskblog.py -> No spaces extra
 #export DEBUG_MODE=1 -> Auto load server changes
 
+#to link multiple routes to same page
+@app.route('/home')
 @app.route('/')
-def hello_world():
+def home():
     return '<h1>Hello, Anand!</h1>'
+
 
 @app.route('/about')
 def about_page():
     return "<h3>It's a About Page </h3>"
+
+#to make it work with "python flaskblog.py" instead of "flask run" add below
+if __name__=="__main__":
+    app.run(debug=True)
